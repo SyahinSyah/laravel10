5cfd71578ae5d64733e33e7f9838ea0ffbc3f3fe
SyahinSyah <syahinsyah7@gmail.com>
2024-02-14 21:46:18 +0800

    add function in controller

--- app/Http/Controllers/SchoolsController.php
+++ app/Http/Controllers/SchoolsController.php
@@ -2,9 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Schools;
 use Illuminate\Http\Request;
 
 class SchoolsController extends Controller
 {
-    //
+
+    // to save the data request 
+   public function store(Request $request){
+        $schools = new Schools();
+        $schools->name = $request->name ;
+        $schools->address1 =  $request->address1 ; 
+        $schools->address2= $request->address2;
+        $schools->address3 = $request->address3; 
+        $schools->email = $request->email;
+        if($schools->save()){
+            return true;
+        }
+   }
+   //to save the data using create method but need to do fill able
+
+   public function storeV2(Request $request)
+   {
+    if(Schools::Create($request->all()))
+    {
+        return true;
+    }
+   }
+
+   public function index()
+   {
+     $schools = Schools::all();
+     return $schools;
+   }
+
+   public function show(Schools $schools)
+   {
+        return $schools; 
+   }
+
+
+
+   public function update(Request $request , Schools $schools)
+   {
+        if($schools->fill($request->all())->save())
+        {
+            return true;
+        }
+   }
+
+
+
 }
--- app/Models/Schools.php
+++ app/Models/Schools.php
@@ -8,7 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Schools extends Model
 {
     use HasFactory;
+    //protected $table "schools"; --> to rename the table 
+    //protected $primaryKey = "isbn_no"; --> set the id name dff from the default
+    //public $incrementing = false; --> set the auto increment to false; 
 
+    protected $fillable = ['name','address1','address2','address3','email'];
 
     public function students()
     {
